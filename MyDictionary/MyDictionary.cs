dd0a0f05e75b683c6a44014d888b71b021970f80
tugru <tugru@DESKTOP-SI6RJ23>
2022-12-18 13:58:37 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,UValue>
+    {
+        KeyValuePair<TKey,UValue>[] items;
+
+        public MyDictionary()
+        {
+            items=new  KeyValuePair<TKey, UValue>[0];
+        }
+
+        public void Add(TKey key,UValue value)
+        {
+            KeyValuePair<TKey, UValue>[] tempArray = items;
+            items = new KeyValuePair<TKey, UValue>[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = new KeyValuePair<TKey, UValue>(key, value);
+        }
+
+        
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(54, "Sakarya");
+            myDictionary.Add(52, "Ordu");
+            myDictionary.Add(45, "Manisa");
+
+            Console.WriteLine(myDictionary.Count);
+            Console.ReadLine();
+        }
+    }
+}
